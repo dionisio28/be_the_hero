fcdcac83b7031fe8897ef08df24dd13e10bef5f0
Dionisio Matos <dionisio.matos@liberali.local>
2020-03-27 23:18:35 -0400

    Primeira Etapa do Front-end

---
+++ front-end/src/pages/Profile/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiPower } from "react-icons/fi";
+import logoImg from "../../assets/logo.svg";
+import "./styles.css";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Be the Hero" />
+        <span>Bem vinda, Ampara</span>
+
+        <Link className="button" to="/incidents/new">
+          Cadastrar novo caso
+        </Link>
+        <button type="button">
+          <FiPower size={18} color="#E02041" />
+        </button>
+      </header>
+    </div>
+  );
+}
